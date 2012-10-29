4eb1057801149e08c671e0e8d134c776635d41dc
Richard van Aalst <richardvanaalst@i-aspect.com>
2012-10-29 15:14:22 +0100

    Initial add of all files.

---
+++ scss/config.rb
@@ -0,0 +1,21 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed.
+http_path       = "/"
+sass_dir        = "./"
+css_dir         = "../css"
+images_dir      = "../img"
+javascripts_dir = "../js"
+
+# Set the environment you're working in.
+environment = :development
+
+# To enable relative paths to assets via compass helper functions.
+relative_assets = true
+
+# You can select your preferred output style here (can be overridden via the command line).
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
+
+# To disable debugging comments that display the original location of your selectors.
+line_comments = false
